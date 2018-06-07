06d1698be39625d06e85f8ac1f132c3cad7f567e
Lauren Malley <laurenymalley@gmail.com>
2018-06-07 16:39:28 -0400

    todo commit

--- todo.js
+++ todo.js
@@ -1,5 +1,20 @@
-const {head} = require('ramda')
-// head is function
-const things = ['foo', 8, null, false, 'bar']
+const { head } = require('ramda')
 
-console.log(head(things))
+const [execNodePath, jsPath, cmd, ...everythingelsegoeshere] = process.argv
+
+//process.argv.forEach(val, index) =>
+//console.log(``
+console.log(handleCmd(cmd))
+function handleCmd(cmd) {
+  // command could be anything like add, init, remove
+  switch (cmd) {
+    case 'add':
+      return 'you asked for the add cmd'
+    case 'rm':
+      return 'you asked for the rm cmd'
+    case 'init':
+      return 'you asked for the init cmd'
+    default:
+      return `${cmd} is not supported`
+  }
+}
